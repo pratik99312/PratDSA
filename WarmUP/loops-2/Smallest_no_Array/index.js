f9ac877a1e08f0ec949b53273a19e63539f86ec7
pratikagale <pratikagale2795@gmail.com>
2026-05-11 11:00:33 +0530

    Smaalest no in an array

--- WarmUP/loops-2/Smallest_no_Array/index.js
+++ WarmUP/loops-2/Smallest_no_Array/index.js
@@ -1,7 +1,7 @@
 // Write a function that returns smallest number in an array
 
 // let arr = [5, 0, 7, 10, 34, 17, 1, 97, 60];
-let arrB = [-9, -19, -3,-21];
+let arrB = [-9, -19, -3, -21, -99, -1, -5, -7, -11];
 function findSmallest(arr) {
   let smallest = arrB[0];
   for (let i = 0; i < arr.length; i++) {
