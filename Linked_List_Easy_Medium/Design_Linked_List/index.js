fedf1c83894e201726e18bb760dcd250fb785f36
pratikagale <pratikagale2795@gmail.com>
2026-06-02 14:24:23 +0530

    Linked List Design

---
+++ Linked_List_Easy_Medium/Design_Linked_List/index.js
@@ -0,0 +1,140 @@
+// write a linked list implementation in JavaScript 
+
+function Node(val){
+    this.value = val;
+    this.next = null;
+}
+
+
+var MyLinkedList = function() {
+    this.head = null;
+    this.size = 0;
+};
+
+/** 
+ * @param {number} index
+ * @return {number}
+ */
+MyLinkedList.prototype.get = function(index) {
+    if(index < 0 || index >= this.size) return -1;
+
+    let curr = this.head;
+    for(let i = 0; i < index; i++){
+        curr = curr.next;
+    }
+    return curr.value;
+};
+
+/** 
+ * @param {number} val
+ * @return {void}
+ */
+MyLinkedList.prototype.addAtHead = function(val) {
+    
+    let newNode = new Node(val);
+    newNode.next = this.head;
+    this.head = newNode;
+    this.size++;
+
+};
+
+/** 
+ * @param {number} val
+ * @return {void}
+ */
+MyLinkedList.prototype.addAtTail = function(val) {
+ 
+    let newNode = new Node(val);
+
+    if(this.head == null){
+        this.head = newNode;
+    } else {
+        let curr = this.head;
+
+        while(curr.next !== null){
+            curr = curr.next;
+        }
+
+        curr.next = newNode;
+    }
+
+    this.size++;
+};
+
+/** 
+ * @param {number} index 
+ * @param {number} val
+ * @return {void}
+ */
+MyLinkedList.prototype.addAtIndex = function(index, val) {
+
+    if(index < 0 || index > this.size) return;
+
+    if(index === 0){
+        this.addAtHead(val);
+        return;
+    }
+
+    if(index === this.size){
+        this.addAtTail(val);
+        return;
+    }
+
+    let newNode = new Node(val);
+
+    let curr = this.head;
+
+    for(let i = 0; i < index - 1; i++){
+        curr = curr.next;
+    }
+
+    newNode.next = curr.next;
+    curr.next = newNode;
+
+    this.size++;
+};
+
+/** 
+ * @param {number} index
+ * @return {void}
+ */
+MyLinkedList.prototype.deleteAtIndex = function(index) {
+    if(index < 0 || index >= this.size) return;
+
+    if(index === 0){
+        this.head = this.head.next;
+    } else {
+        let curr = this.head;
+
+        for(let i = 0; i < index - 1; i++){
+            curr = curr.next;
+        }
+
+        curr.next = curr.next.next;
+    }
+
+    this.size--;
+};
+
+/** 
+ * Your MyLinkedList object will be instantiated and called as such:
+ * var obj = new MyLinkedList()
+ * var param_1 = obj.get(index)
+ * obj.addAtHead(val)
+ * obj.addAtTail(val)
+ * obj.addAtIndex(index,val)
+ * obj.deleteAtIndex(index)
+ */
+
+
+// dry run
+// let obj = new MyLinkedList();
+// obj.addAtHead(1); // linked list is now 1
+// obj.addAtTail(3); // linked list is now 1 -> 3
+// obj.addAtIndex(1, 2); // linked list is now 1 -> 2 -> 3
+// console.log(obj.get(1)); // returns 2
+// obj.deleteAtIndex(1); // linked list is now 1 -> 3
+// console.log(obj.get(1)); // returns 3
+
+// Time Complexity: O(n) for get, addAtIndex, deleteAtIndex and O(1) for addAtHead and addAtTail
+// Space Complexity: O(n) where n is the number of nodes in the linked list.    
--- Searching_And_Sorting_EasyMedium/MergeSort/index.js
+++ Searching_And_Sorting_EasyMedium/MergeSort/index.js
@@ -4,33 +4,32 @@
  * @param {number[]} nums
  * @return {number[]}
  */
-var sortArray = function(arr) {
-     if(arr.length <= 1) return arr;
+var sortArray = function (arr) {
+  if (arr.length <= 1) return arr;
 
-     let mid = Math.floor(arr.length/2);
-     let left = sortArray(arr.slice(0,mid));
-     let right = sortArray(arr.slice(mid));
+  let mid = Math.floor(arr.length / 2);
+  let left = sortArray(arr.slice(0, mid));
+  let right = sortArray(arr.slice(mid));
 
-     return merge(left,right);
+  return merge(left, right);
 };
 
+function merge(left, right) {
+  let res = [];
+  let i = 0;
+  let j = 0;
 
-function merge(left,right){
-    let res= [];
-    let i = 0;
-    let j = 0;
-
-    while(i < left.length && j < right.length){
-        if(left[i] < right [j]){
-            res.push(left[i]);
-            i++;
-        }else{
-            res.push(right[j]);
-            j++;
-        }
+  while (i < left.length && j < right.length) {
+    if (left[i] < right[j]) {
+      res.push(left[i]);
+      i++;
+    } else {
+      res.push(right[j]);
+      j++;
     }
+  }
 
-    return[ ...res, ...left.slice(i),  ...right.slice(j)];
+  return [...res, ...left.slice(i), ...right.slice(j)];
 }
 
 const arr = [5, 2, 3, 1];
@@ -47,4 +46,4 @@ console.log("Merge Sort Array is::", result);
 
 // So the final output will be Merge Sort Array is:: [1, 2, 3, 5]
 
-// This is the optimized solution with O(n) space complexity and O(n log n) time complexity.                
+// This is the optimized solution with O(n) space complexity and O(n log n) time complexity.
